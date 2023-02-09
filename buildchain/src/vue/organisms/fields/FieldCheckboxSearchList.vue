3ab7a94213b822b03686e87814b67f28e806e34a
Stefanie Gevaert <>
2023-02-09 11:58:49 +0100

    build: frontend build of the fix select all

--- buildchain/src/vue/organisms/fields/FieldCheckboxSearchList.vue
+++ buildchain/src/vue/organisms/fields/FieldCheckboxSearchList.vue
@@ -63,55 +63,55 @@ onMounted(() => {
 </script>
 
 <template>
-    <section class="w-full pb-2 bg-gray-100 rounded-md">
-        <div class="p-2 rounded-md">
-            <input 
-                v-model="search" 
-                type="text" 
-                class="bg-white w-full py-2 px-2 rounded-md border border-sate-200"
-                :placeholder="translations['placeholder'] ?? ''"
-            >
-        </div>
-        <div
-            v-if="filteredOptions.length > 0"
-            class="max-h-[400px] overflow-scroll"
-        >
-            <div class="px-2">
-                <label class="flex items-center p-1 cursor-pointer rounded-md hover:bg-slate-200">
-                    <input 
-                        v-model="selectAll" 
-                        type="checkbox"
-                        @change="toggle"
-                    >
-                    <span>Select all</span>
-                </label>
-            </div>
+  <section class="w-full pb-2 bg-gray-100 rounded-md">
+    <div class="p-2 rounded-md">
+      <input 
+        v-model="search" 
+        type="text" 
+        class="bg-white w-full py-2 px-2 rounded-md border border-sate-200"
+        :placeholder="translations['placeholder'] ?? ''"
+      >
+    </div>
+    <div
+      v-if="filteredOptions.length > 0"
+      class="max-h-[400px] overflow-scroll"
+    >
+      <div class="px-2">
+        <label class="flex items-center p-1 cursor-pointer rounded-md hover:bg-slate-200">
+          <input 
+            v-model="selectAll" 
+            type="checkbox"
+            @change="toggle"
+          >
+          <span>Select all</span>
+        </label>
+      </div>
 
-            <div
-                v-for="(option, i) in filteredOptions"
-                :key="option.value"
-                class="px-2"
-            >
-                <label class="flex items-center p-1 cursor-pointer rounded-md hover:bg-slate-200">
-                    <!-- :checked="checked.find(checkbox => checkbox.value === option.value)" -->
-                    <input
-                        :id="`field-checkbox${id}-${i}`"
-                        type="checkbox"
-                        :name="`fields[${option.name}][]`"
-                        :checked="checked.find(checkbox => checkbox.value === option.value)"
-                        :value="option.value"
-                        @change="onUpdateChecked"
-                    >
-                    <span>{{ option.label }}</span>
-                </label>
-            </div>
-        </div>
+      <div
+        v-for="(option, i) in filteredOptions"
+        :key="option.value"
+        class="px-2"
+      >
+        <label class="flex items-center p-1 cursor-pointer rounded-md hover:bg-slate-200">
+          <!-- :checked="checked.find(checkbox => checkbox.value === option.value)" -->
+          <input
+            :id="`field-checkbox${id}-${i}`"
+            type="checkbox"
+            :name="`fields[${option.name}][]`"
+            :checked="checked.find(checkbox => checkbox.value === option.value)"
+            :value="option.value"
+            @change="onUpdateChecked"
+          >
+          <span>{{ option.label }}</span>
+        </label>
+      </div>
+    </div>
 
-        <div
-            v-if="filteredOptions.length === 0"
-            class="px-2 pt-2"
-        >
-            <span class="p-1">{{ translations['noResults'] ?? '-' }}</span>
-        </div>
-    </section>
+    <div
+      v-if="filteredOptions.length === 0"
+      class="px-2 pt-2"
+    >
+      <span class="p-1">{{ translations['noResults'] ?? '-' }}</span>
+    </div>
+  </section>
 </template>
